f0ad391c243b6eabefcaaca4ed3c1d532f89d806
Vaidas <vaidasl07@gmail.com>
2023-11-23 17:39:02 +0200

    Added fixes for module instalation and added view for records

--- models/models.py
+++ models/models.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields
 
-class saleOrder(models.Model):
-    _inherit = 'sale.order'
+class Information(models.Model):
+    _name = 'save_information.save_information'
+    _description = 'Save stuff information'
 
-    spokentoclient = fields.Boolean('Spoken to client', required=True)
+    name = fields.Char(string="Title", required=True, help="Save information")
+    description = fields.Text()
+
