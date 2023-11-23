776e8cccfea99d57e2ececa7700d43c1591e47d1
Vaidas <vaidasl07@gmail.com>
2023-11-23 23:19:23 +0200

    Added small fixes for names

--- models/models.py
+++ models/models.py
@@ -5,6 +5,6 @@ class Information(models.Model):
     _name = 'save_information.save_information'
     _description = 'Save stuff information'
 
-    name = fields.Char(string="Title", required=True, help="Save information")
-    description = fields.Text(string='description')
+    name = fields.Char(string="Name", required=True, help="Save information")
+    description = fields.Text(string='Description')
     company_id = fields.Many2one('res.company', string='Company', required=True)
