176a31ae39c0cb23f7ffa59fd3e5bfc93e1c6d4c
Vaidas <vaidasl07@gmail.com>
2023-11-23 23:41:47 +0200

    Added file in different directory

--- __manifest__.py
+++ __manifest__.py
@@ -27,7 +27,7 @@
         'security/ir.model.access.csv',
         'security/security.xml',
         'views/save_information.xml',
-        'views/report_document.xml',
+        'report/report_document.xml',
        # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
