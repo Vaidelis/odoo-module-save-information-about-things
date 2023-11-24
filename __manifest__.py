77ff6f6e5b2cb0132796f5eee95f32a0f876a5b1
Vaidas <vaidasl07@gmail.com>
2023-11-24 12:59:24 +0200

    Updated manifest.py file

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "save_information",
+    'name': "Save information",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Module for saving information about items""",
 
     'description': """
-        Long description of module's purpose
+        Module can save information about items and declare for which company item belongs
     """,
 
-    'author': "My Company",
+    'author': "Vaidas",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
@@ -24,12 +23,9 @@
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
+        #'security/ir.model.access.csv',
         'security/security.xml',
         'views/save_information.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+        'wizard/information_report_view.xml',
     ],
 }
